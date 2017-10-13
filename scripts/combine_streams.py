ad4bcaa5b74a39cc3f24f8bcff904f5793468fa4
Nico de Poel <ndepoel@gmail.com>
2017-10-13 14:13:59 +0200

    Added Python scripts for downloading and combining video streams from a streaming server

---
+++ scripts/combine_streams.py
@@ -0,0 +1,283 @@
+#!/bin/env python
+
+import sys, argparse
+import os, os.path
+import re
+from datetime import datetime, timedelta
+import pytz
+import subprocess
+import math
+
+default_timezone = pytz.timezone('Europe/Amsterdam')
+ffprobe = 'ffmpeg/bin/ffprobe'
+ffmpeg = 'ffmpeg/bin/ffmpeg'
+font_file = 'FreeSerif.ttf'
+
+argparser = argparse.ArgumentParser(description='Tool to process recorded video streams and combine multiple parallel streams into a single video.')
+argparser.add_argument('-t', '--timezone', default='Europe/Amsterdam', help='Time zone to process timestamps with.')
+argparser.add_argument('--ffprobe', default='ffmpeg/bin/ffprobe', help='Path to ffprobe executable.')
+argparser.add_argument('--ffmpeg', default='ffmpeg/bin/ffmpeg', help='Path to ffmpeg executable.')
+argparser.add_argument('-f', '--font_file', default='FreeSerif.ttf', help='Font file to use for text overlays.')
+argparser.add_argument('-s', '--preferred_stream', default='', help='Name of the stream to give precedence, i.e. that will be placed first and used for sourcing the audio stream if possible.')
+argparser.add_argument('-i', '--input_dir', help='Directory to look for input stream video files.', required=True)
+argparser.add_argument('-o', '--output_dir', help='Directory to place output video files.', required=True)
+argparser.add_argument('--dry-run', action='store_true', help='Whether or not to do a dry run. Dry runs do not actually perform any video encoding.')
+
+def format_timedelta(td):
+    secs = td.total_seconds()
+    return '{:02}:{:02}:{:02}.{:03}'.format(int(secs) // 3600, int(secs) % 3600 // 60, int(secs) % 60, int((secs - math.floor(secs)) * 1000))
+
+class VideoDescriptor(object):
+    def __init__(self, filename):
+        self.filename = filename
+    
+        # Extract stream name and start timestamp from the video filename
+        match = re.search(r'(\w+)-(\d{4})(\d{2})(\d{2})-(\d{2})(\d{2})(\d{2})(-(\d{3}))?', filename)
+        if not match:
+            raise ValueError('Invalid video filename: ' + filename)
+        
+        self.stream_name = match.group(1)
+        (year, month, day, hour, minute, second) = [int(x) for x in match.group(2, 3, 4, 5, 6, 7)]
+        millisecond = int(match.group(9)) if match.group(9) else 0
+        self.start_time = datetime(year, month, day, hour, minute, second, millisecond * 1000, default_timezone)
+        
+    def analyze_video(self):
+        # Use ffprobe to extract the video's duration and calculate the video's exact end timestamp
+        probe = subprocess.check_output([ffprobe, self.filename], stderr=subprocess.STDOUT)
+        match = re.search(r'Duration: (\d+):(\d+):(\d+)\.(\d+),', probe)
+        if not match:
+            raise ValueError('Could not probe video info for file: ' + self.filename)
+            
+        (hours, minutes, seconds) = [int(x) for x in match.group(1, 2, 3)]
+        fraction = match.group(4)
+        milliseconds = int(fraction + ('0' * (3 - len(fraction))))
+        
+        self.duration = timedelta(hours = hours, minutes = minutes, seconds = seconds, milliseconds = milliseconds)
+        self.end_time = self.start_time + self.duration
+        
+    def create_video_input(self, start_time, duration):
+        input = []
+        
+        # Add starting offset (if applicable)
+        offset = start_time - self.start_time
+        if offset.total_seconds() > 0.0:
+            input.extend(['-ss', format_timedelta(offset)])
+            
+        # Add duration
+        input.extend(['-t', format_timedelta(duration)])
+        
+        # Add input filename
+        input.extend(['-i', self.filename])
+        return input
+        
+    def create_filter(self, index, scale, pad, out_name):
+        filters = ['setpts=PTS-STARTPTS']
+        if scale:
+            filters.append('scale={}:{}'.format(*scale))
+            
+        filters.append(self._create_text_overlay())
+        
+        if pad:
+            filters.append('pad={}:{}:{}:{}'.format(*pad))
+            
+        return '[{}:v]'.format(index) + ','.join(filters) + '[{}]'.format(out_name)
+        
+    def _create_text_overlay(self):
+        args = {
+            'fontfile': "'{}'".format(font_file),
+            'text': "'{}'".format(self.stream_name),
+            'fontcolor': 'white',
+            'fontsize': 96,
+            'box': 1,
+            'boxcolor': 'black@0.5',
+            'boxborderw': 5,
+            'x': 5,
+            'y': '(h-text_h-5)',
+        }
+        
+        instructions = ':'.join(['{}={}'.format(k, str(args[k])) for k in args])
+        return 'drawtext={}'.format(instructions)
+        
+    def __str__(self):
+        return '{}: {} to {}'.format(self.stream_name, self.start_time, self.end_time)
+
+class VideoSegment(object):
+    def __init__(self, start_time, videos, preferred_stream):
+        self.start_time = start_time
+        self.videos = videos
+        self.preferred_stream = preferred_stream
+        
+    def start_video(self, vid):
+        self.end_time = vid.start_time
+        new_vids = list(self.videos)
+        new_vids.append(vid)
+        return VideoSegment(vid.start_time, new_vids, self.preferred_stream)
+        
+    def end_video(self, vid):
+        self.end_time = vid.end_time
+        new_vids = list(self.videos)
+        new_vids.remove(vid)
+        return VideoSegment(vid.end_time, new_vids, self.preferred_stream)
+        
+    def duration(self):
+        if self.start_time and self.end_time:
+            return self.end_time - self.start_time
+        else:
+            return None
+        
+    def sorted_videos(self):
+        # Sort videos so that "my" video stream always comes first (for audio selection purposes)
+        return sorted(self.videos, key = lambda v: -1 if v.stream_name == self.preferred_stream else 1)
+        
+    def create_video_inputs(self):
+        inputs = []
+        for vid in self.sorted_videos():
+            inputs.extend(vid.create_video_input(self.start_time, self.duration()))
+            
+        return inputs
+        
+    def create_video_filter(self):
+        steps = []
+        
+        vids = self.sorted_videos()
+        num_videos = len(vids)
+        
+        if num_videos == 1:
+            #steps.append(vids[0].create_filter(0, (1920, 1080), None, 'v'))
+            steps.append(vids[0].create_filter(0, (3840, 2160), None, 'v'))
+        elif num_videos == 2:
+            #steps.append(vids[0].create_filter(0, (1920, 1080), (2560, 1080, 0, 0), 'top'))
+            #steps.append(vids[1].create_filter(1, (1920, 1080), (2560, 1080, 640, 0), 'bottom'))
+            steps.append(vids[0].create_filter(0, (1920, 1080), (3840, 1080, 640, 0), 'top'))
+            steps.append(vids[1].create_filter(1, (1920, 1080), (3840, 1080, 1280, 0), 'bottom'))
+            steps.append('[top][bottom]vstack[v]')
+        elif num_videos == 3:
+            steps.append(vids[0].create_filter(0, (1920, 1080), None, 'topleft'))
+            steps.append(vids[1].create_filter(1, (1920, 1080), None, 'topright'))
+            steps.append(vids[2].create_filter(2, (1920, 1080), (3840, 1080, 960, 960), 'bottom'))
+            steps.append('[topleft][topright]hstack[top]')
+            steps.append('[top][bottom]vstack[v]')
+        elif num_videos >= 4:
+            steps.append(vids[0].create_filter(0, (1920, 1080), None, 'topleft'))
+            steps.append(vids[1].create_filter(1, (1920, 1080), None, 'topright'))
+            steps.append(vids[2].create_filter(2, (1920, 1080), None, 'bottomleft'))
+            steps.append(vids[3].create_filter(3, (1920, 1080), None, 'bottomright'))
+            steps.append('[topleft][topright]hstack[top]')
+            steps.append('[bottomleft][bottomright]hstack[bottom]')
+            steps.append('[top][bottom]vstack[v]')
+        
+        return ['-filter_complex', ';'.join(steps)]
+        
+    def output_filename(self):
+        datestr = self.start_time.strftime('%Y-%m-%d %H-%M-%S')
+        names = '-'.join([v.stream_name for v in self.videos])
+        return '{} {}.mkv'.format(datestr, names)
+        
+    def __str__(self):
+        result = 'Segment duration {}'.format(self.duration())
+        if self.videos:
+            for vid in self.videos:
+                result += '\n\t' + str(vid)
+                
+        return result
+        
+def collect_videos(work_dir):
+    result = []
+    for filename in os.listdir(work_dir):
+        (_, ext) = os.path.splitext(filename)
+        if ext != '.flv':
+            continue
+            
+        fullpath = os.path.realpath(os.path.join(work_dir, filename))
+        vid = VideoDescriptor(fullpath)
+        vid.analyze_video()
+        result.append(vid)
+        
+    return result
+    
+def create_timeline(vids, preferred_stream = ''):
+    segments = []
+    if not vids:
+        return segments
+    
+    sorted_by_start = sorted(vids, key = lambda v: v.start_time)
+    sorted_by_end = sorted(vids, key = lambda v: v.end_time)
+    
+    start_vid = sorted_by_start.pop(0)
+    current_segment = VideoSegment(start_vid.start_time, [start_vid], preferred_stream)
+    segments.append(current_segment)
+    
+    # Go through the videos in chronological order.
+    # Each change in a video's state (i.e. a video starts or ends) creates a new video segment on the timeline.
+    while sorted_by_start or sorted_by_end:
+        next_start = sorted_by_start[0] if sorted_by_start else None
+        next_end = sorted_by_end[0] if sorted_by_end else None
+        if next_start and next_end:
+            if next_start.start_time < next_end.end_time:
+                current_segment = current_segment.start_video(next_start)
+                segments.append(current_segment)
+                sorted_by_start.remove(next_start)
+            else:
+                current_segment = current_segment.end_video(next_end)
+                segments.append(current_segment)
+                sorted_by_end.remove(next_end)
+        elif next_start:
+            current_segment = current_segment.start_video(next_start)
+            segments.append(current_segment)
+            sorted_by_start.remove(next_start)
+        elif next_end:
+            current_segment = current_segment.end_video(next_end)
+            segments.append(current_segment)
+            sorted_by_end.remove(next_end)
+    
+    return segments
+    
+def encode_segment(segment, output_dir, dry_run = False):
+    if not segment.videos or not segment.duration():
+        raise ValueError("Trying to an encode an empty or unclosed video segment:\n" + str(segment))
+        
+    print('Encoding video segment: {}'.format(segment.output_filename()))
+    
+    cmd = [ffmpeg, '-fflags', '+genpts']
+    cmd.extend(segment.create_video_inputs())
+    cmd.extend(segment.create_video_filter())
+    cmd.extend(['-map', '[v]', '-map', '0:a'])    # Stream mapping
+    cmd.extend(['-c:v', 'h264_nvenc', '-preset', 'hq', '-qp', '25'])#, '-vsync', '0']) # Video encoding settings
+    #cmd.extend(['-force_key_frames', "expr:gte(t,n_forced*3)", '-forced-idr', '1'])    # Forced keyframe generation
+    cmd.extend(['-c:a', 'aac']) # Audio encoding settings
+    cmd.append(os.path.join(output_dir, segment.output_filename()))
+    
+    print(' '.join(cmd))
+    
+    if not dry_run:
+        subprocess.check_call(cmd)
+    
+def main(input_dir, output_dir, preferred_stream = '', dry_run = False):
+    vids = collect_videos(input_dir)
+    for vid in vids:
+        print('{}: {} to {} (duration: {})'.format(vid.stream_name, vid.start_time, vid.end_time, vid.duration))
+    
+    segments = create_timeline(vids, preferred_stream)
+    
+    # Filter out empty segments and segments shorter than 2 minutes
+    segments = [s for s in segments if s.videos and s.duration().total_seconds() >= 120]
+    for seg in segments:
+        print(str(seg))
+        
+    for seg in segments:
+        encode_segment(seg, output_dir, dry_run)
+        print('')
+        
+    return 0
+
+if __name__ == '__main__':
+    args = argparser.parse_args()
+    
+    # Assign static global variables
+    default_timezone = pytz.timezone(args.timezone)
+    ffprobe = args.ffprobe
+    ffmpeg = args.ffmpeg
+    font_file = args.font_file
+    
+    res = main(args.input_dir, args.output_dir, args.preferred_stream, args.dry_run)
+    sys.exit(res)
---
+++ scripts/download_streams.py
@@ -0,0 +1,236 @@
+from __future__ import print_function
+import sys, argparse
+import os, os.path
+import tempfile
+import paramiko
+import re
+from datetime import datetime
+import pytz
+import hashlib
+import getpass
+
+argparser = argparse.ArgumentParser(description='Tool to automatically move video stream files from an SFTP server to a local disk and retrieve each file\'s timestamp with millisecond precision.')
+argparser.add_argument('-s', '--hostname', help='Hostname of the server to retrieve media files from.', required=True)
+argparser.add_argument('-u', '--username', help='Username to log into the server. This user needs to be a sudoer.', required=True)
+argparser.add_argument('-p', '--password', help='Password to log the user into the server with.')
+argparser.add_argument('-m', '--media_path', help='Pathname on the remote host to retrieve the media files from. This needs to be on an ext4-formatted file system.', required=True)
+argparser.add_argument('-o', '--output_dir', help='Local directory to download the media files to.', required=True)
+
+class Connection(object):
+    """Connects and logs into the specified hostname. 
+    Arguments that are not given are guessed from the environment.""" 
+
+    def __init__(self,
+                 host,
+                 username = None,
+                 private_key = None,
+                 password = None,
+                 port = 22,
+                 ):
+        self._sftp_live = False
+        self._sftp = None
+        if not username:
+            username = os.environ['LOGNAME']
+
+        # Log to a temporary file.
+        templog = tempfile.mkstemp('.txt', 'ssh-')[1]
+        paramiko.util.log_to_file(templog)
+
+        # Begin the SSH transport.
+        self._transport = paramiko.Transport((host, port))
+        self._tranport_live = True
+        # Authenticate the transport.
+        if password:
+            # Using Password.
+            self._transport.connect(username = username, password = password)
+        else:
+            # Use Private Key.
+            if not private_key:
+                # Try to use default key.
+                if os.path.exists(os.path.expanduser('~/.ssh/id_rsa')):
+                    private_key = '~/.ssh/id_rsa'
+                elif os.path.exists(os.path.expanduser('~/.ssh/id_dsa')):
+                    private_key = '~/.ssh/id_dsa'
+                else:
+                    raise TypeError, "You have not specified a password or key."
+
+            private_key_file = os.path.expanduser(private_key)
+            rsa_key = paramiko.RSAKey.from_private_key_file(private_key_file)
+            self._transport.connect(username = username, pkey = rsa_key)
+
+    def _sftp_connect(self):
+        """Establish the SFTP connection."""
+        if not self._sftp_live:
+            self._sftp = paramiko.SFTPClient.from_transport(self._transport)
+            self._sftp_live = True
+
+    def get(self, remotepath, localpath = None, callback = None):
+        """Copies a file between the remote host and the local host."""
+        if not localpath:
+            localpath = os.path.split(remotepath)[1]
+        self._sftp_connect()
+        self._sftp.get(remotepath, localpath, callback)
+
+    def put(self, localpath, remotepath = None):
+        """Copies a file between the local host and the remote host."""
+        if not remotepath:
+            remotepath = os.path.split(localpath)[1]
+        self._sftp_connect()
+        self._sftp.put(localpath, remotepath)
+
+    def listdir(self, remotepath = None):
+        """Lists the contents of a remote directory path."""
+        if not remotepath:
+            remotepath = '.'
+        self._sftp_connect()
+        return self._sftp.listdir(remotepath)
+
+    def removefile(self, remotepath):
+        """Removes a file from the remote host."""
+        self._sftp_connect()
+        self._sftp.remove(remotepath)
+        
+    def stat(self, remotepath):
+        """Retrieve information abotu a file on the remote system."""
+        self._sftp_connect()
+        return self._sftp.stat(remotepath)
+        
+    def execute(self, command):
+        """Execute the given commands on a remote machine."""
+        channel = self._transport.open_session()
+        channel.exec_command(command)
+        output = channel.makefile('rb', -1).readlines()
+        if output:
+            return output
+        else:
+            return channel.makefile_stderr('rb', -1).readlines()
+
+    def close(self):
+        """Closes the connection and cleans up."""
+        # Close SFTP Connection.
+        if self._sftp_live:
+            self._sftp.close()
+            self._sftp_live = False
+        # Close the SSH Transport.
+        if self._tranport_live:
+            self._transport.close()
+            self._tranport_live = False
+
+    def __del__(self):
+        """Attempt to clean up if not explicitly closed."""
+        self.close()
+
+def md5sum(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+        
+prev_mbs = -1
+
+def print_progress(count, total):
+    global prev_mbs
+
+    mbs = count / 1048576
+    if mbs < prev_mbs:
+        prev_mbs = -1
+    
+    if mbs > prev_mbs:
+        total_mbs = total / 1048576
+        progress = round(float(count * 100) / total)
+        print("{} of {} MB ({}%)\r".format(mbs, total_mbs, int(progress)), end='')
+        prev_mbs = mbs
+        
+def main(serverhost, username, password, mediapath, output_dir):
+    # Connect to the SFTP server
+    myssh = Connection(serverhost, username=username, password=password)
+
+    # Figure out the disk device that the media path is located on
+    remotedevice = None
+    for line in myssh.execute('df --output=source {} | grep ^/'.format(mediapath)):
+        remotedevice = line.strip()
+        
+    if not remotedevice:
+        print('Could not find the remote disk device that {} is located on!'.format(mediapath))
+        myssh.close()
+        sys.exit(1)
+    
+    print('{} is located on disk device {}'.format(mediapath, remotedevice))
+    
+    # Figure out the timezone that the server is set to
+    servertz = None
+    for line in myssh.execute('timedatectl | grep -i zone'):
+        match = re.search(r'one:\s+([^\s]+)\s+\(', line)
+        if match:
+            servertz = match.group(1)
+            
+    if not servertz:
+        print('Could not find the timezone that the server is set to!')
+        myssh.close()
+        sys.exit(1)
+        
+    print('Server is set to timezone: {}'.format(servertz))
+    
+    # Get a list of FLV movie files from the media folder on the remote host
+    remote_files = myssh.listdir(mediapath)
+    for remote_file in remote_files:
+        remotepath = mediapath + '/' + remote_file
+        (_, ext) = os.path.splitext(remotepath)
+        if ext != '.flv':
+            continue
+            
+        print(remotepath)
+        
+        # Calculate the remote file's MD5 sum so we can verify the locally downloaded version later
+        remote_md5sum = None
+        for line in myssh.execute('md5sum ' + remotepath):
+            match = re.match(r'(\w+)\s', line)
+            if match:
+                remote_md5sum = match.group(1)
+                break
+        
+        print('MD5: ' + remote_md5sum)
+        
+        # Retrieve the remote file's creation time with nanosecond precision from the server
+        crtime = None
+        cmd = 'echo \'{}\' | sudo -S debugfs -R "stat <$(stat -c %i {})>" {} | grep crtime'.format(password, remotepath, remotedevice)
+        for line in myssh.execute(cmd):
+            match = re.match(r'crtime: 0x([0-9a-f]+):([0-9a-f]+)', line)
+            if match:
+                seconds = int(match.group(1), 16)
+                nanosecs = int(match.group(2), 16) / 4
+                crtime = datetime.fromtimestamp(seconds, pytz.timezone(servertz))
+                crtime = crtime.replace(microsecond = nanosecs / 1000)
+                break
+                
+        print('File creation time: ' + str(crtime))
+
+        # Determine the local filename for this file, which includes milliseconds in the timestamp
+        (filename, ext) = os.path.splitext(remote_file)
+        local_filename = os.path.join(output_dir, filename + '-{:03}'.format(crtime.microsecond / 1000) + ext)
+        
+        # Download the file if it doesn't exist locally or is incomplete
+        if not os.path.exists(local_filename) or md5sum(local_filename) != remote_md5sum:
+            print('Downloading {} to local file: {}'.format(remote_file, local_filename))
+            myssh.get(remotepath, local_filename, print_progress)
+        
+        # If the file was already previously downloaded by its original name, rename it
+        if os.path.exists(remote_file):
+            print('Renaming {} => {}'.format(remote_file, local_filename))
+            os.rename(remote_file, local_filename)
+            
+        # Delete the file from the server if it was succesfully downloaded
+        if md5sum(local_filename) == remote_md5sum:
+            print('Deleting remote file: {}'.format(remotepath))
+            myssh.removefile(remotepath)
+        
+        print('')
+
+    myssh.close()
+
+# start the ball rolling.
+if __name__ == "__main__":
+    args = argparser.parse_args()
+    password = args.password if args.password else getpass.getpass()    
+    main(args.hostname, args.username, password, args.media_path, args.output_dir)
